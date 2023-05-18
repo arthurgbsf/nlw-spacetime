da8db0a0a906fb409362e0c3517b3a591878ef48
arthurgbsf <arthurgbsf@gmail.com>
2023-05-17 21:37:56 -0300

    Class 02: First 20 min.

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50">
-      <h1 className="text-2xl p-6">Hello World</h1>
+      <h1 className="p-6 font-sans text-2xl">Hello World</h1>
     </div>
   )
 }
