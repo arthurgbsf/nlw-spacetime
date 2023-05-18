6c0bb9f48bb721eafa8a37504cd3e0e0b7c0eef2
arthurgbsf <arthurgbsf@gmail.com>
2023-05-17 22:04:02 -0300

    Class 02: First 20 min.

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50">
-      <h1 className="p-6 font-sans text-2xl">Hello World</h1>
+      <h1 className="p-6 font-sans text-2xl font-bold">Sua Cápsula do tempo</h1>
     </div>
   )
 }
